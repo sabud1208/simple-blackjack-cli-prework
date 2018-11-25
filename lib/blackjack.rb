39210d5e088884d0dcc786e6891310fa95555fb6
Stephanie Messad <sabud1208@gmail.com>
2018-11-25 07:46:57 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -51,13 +51,4 @@ def invalid_command
 end
 
 
-def runner
-  welcome
-  card_total = initial_round
-  until card_total > 21
-    card_total = hit?(card_total)
-    display_card_total(card_total)
-  end
-  end_game(card_total)
-end
-    s
+
